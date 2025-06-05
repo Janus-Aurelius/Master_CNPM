4bcff0d839b944f3e005931c0e45c517a68efc9a
conbobietbayy <146724850+conbobietbayy@users.noreply.github.com>
2025-06-05 21:58:01 +0700

    Revert "hihi"
    This reverts commit 90a05e742e215ce7cd0ff6641d7224baf74283d1.

--- master_frontend_tsx/src/financial_dpm_pages/tuitionAdjustment_financial.tsx
+++ master_frontend_tsx/src/financial_dpm_pages/tuitionAdjustment_financial.tsx
@@ -1,19 +1,17 @@
-import { SelectChangeEvent } from '@mui/material/Select';
-import { useState, useMemo } from "react";
-import { ThemeLayout } from "../styles/theme_layout.tsx";
-import { User } from "../types";
-import UserInfo from "../components/UserInfo";
-import {
-    Box, Paper, TextField, Typography, Button,
-    Select, MenuItem, FormControl, InputLabel,
-    Table, TableBody, TableCell, TableContainer, TableHead, TableRow,
-    Grid, InputAdornment, Dialog, DialogTitle, DialogContent, 
-    DialogActions, IconButton, Divider, Tabs, Tab, Chip
-} from "@mui/material";
-import SearchIcon from '@mui/icons-material/Search';
-import FilterListIcon from '@mui/icons-material/FilterList';
-import SaveIcon from '@mui/icons-material/Save';
-import EditIcon from '@mui/icons-material/Edit';
-import AddIcon from '@mui/icons-material/Add';
-import DeleteIcon from '@mui/icons-material/Delete';
+import {ThemeLayout} from "../styles/theme_layout.tsx";
+import Typography from "@mui/material/Typography";
+import {User} from "../types";
 
+interface FinancialPageProps {
+    user: User | null;
+    onLogout: () => void;
+}
+
+
+export default function TuitionAdjustment({ onLogout }: FinancialPageProps) {
+    return (
+        <ThemeLayout role="financial" onLogout={onLogout}>
+            <Typography variant="h4">Quản lý học phí</Typography>
+        </ThemeLayout>
+    );
+}
